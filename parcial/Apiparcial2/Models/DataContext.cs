8d1b4812aae24c4ef5791de427b062978a1bc00f
Jacqueline montano <Jacqueline montano@DESKTOP-9LLNFII>
2019-04-06 08:07:03 -0400

    Ci=ommit con modelo web api

---
+++ parcial/Apiparcial2/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Apiparcial2.Models
+{
+    public class DataContext:DbContext
+
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Apiparcial2.Models.Product> Products { get; set; }
+    }
+}
---
+++ parcial/Apiparcial2/Models/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Apiparcial2.Models
+
+
+{
+    public enum UniType
+    {
+        Botella,
+        caja,
+        litro
+    }
+
+    public enum StatusType
+    {
+        Activo, 
+        Inactivo
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public UniType Unit { get; set; }
+
+        public StatusType Status { get; set; }
+    }
+}
